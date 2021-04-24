7e5cd1bee2ade5882dd65cd841f518d96e41c4a6
dvaibhavim <deshpamdevai@gmail.com>
2021-04-24 09:49:19 +0530

    added method to search weather

--- server.py
+++ server.py
@@ -1,1 +1,14 @@
-'# server file for weather app'
+# server file for weather app
+from flask import Flask, render_template
+
+#Api key to establish handshake between Accu weather
+API_KEY = "qQgGRb1A0A6R5HvKx7T37JCKdaAYG8GT"
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def search():
+	return render_template("weather.html")
+
+
