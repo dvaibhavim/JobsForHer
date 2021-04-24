6c13120728f5a62f42309df79d63da6b3e977b28
dvaibhavim <deshpamdevai@gmail.com>
2021-04-24 11:01:21 +0530

    added code to handle errors and error page

--- server.py
+++ server.py
@@ -10,38 +10,41 @@ app = Flask(__name__)
 
 @app.route('/')
 def search():
-	return render_template("search.html")
+    return render_template("search.html")
 
-	
+    
 
 @app.route('/get-weather', methods=['GET'])
 def get_weather_data():
     """Search location data from ACCU weather site and return weather details."""
-    location = request.args.get('location')
-    context = {'apikey':API_KEY, 'q':location, 'language':'en-us'}
-    response = requests.get('http://dataservice.accuweather.com/locations/v1/search',
-                                 params=context)
+    try:
+        location = request.args.get('location')
+        context = {'apikey':API_KEY, 'q':location, 'language':'en-us'}
+        response = requests.get('http://dataservice.accuweather.com/locations/v1/search',
+                                     params=context)
 
-    data = response.json()
-    print(response, data, location)
-    
-    location_key = data[0]['Key']
-    country = data[0]['Country']['LocalizedName']
-    #call weather Api to get the location's data. For getting any data from Accuweather, it's necessary to pas location.
-    response1 = requests.get('http://dataservice.accuweather.com/currentconditions/v1/%s' % location_key, params=context)
+        data = response.json()
+        print(response, data, location)
+        
+        location_key = data[0]['Key']
+        country = data[0]['Country']['LocalizedName']
+        #call weather Api to get the location's data. For getting any data from Accuweather, it's necessary to pas location.
+        response1 = requests.get('http://dataservice.accuweather.com/currentconditions/v1/%s' % location_key, params=context)
 
-    # Process the JSON object into a list of results
-    data1 = response1.json()
-    text = data1[0]['WeatherText']
-    temperature = data1[0]['Temperature']['Imperial']['Value']
-    #show the results of the api in weather.html and pass location details.
-    return render_template(
-        'weather.html',       
-        location=location,
-        Text=text,
-        Temperature=temperature,
-        country=country
-    )
+        # Process the JSON object into a list of results
+        data1 = response1.json()
+        text = data1[0]['WeatherText']
+        temperature = data1[0]['Temperature']['Imperial']['Value']
+        #show the results of the api in weather.html and pass location details.
+        return render_template(
+            'weather.html',       
+            location=location,
+            Text=text,
+            Temperature=temperature,
+            country=country
+        )
+    except Exception as e:
+        return render_template('error_page.html',erro=str(e))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
